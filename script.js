44e7ab236ed2155bda456f164ccad3974ab61958
COUSIN <B1037353@live.tees.ac.uk>
2022-04-11 15:26:43 +0100

    17 Changes - CSS file, Nav, Footer, Feedback page

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Hamburger JS */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
